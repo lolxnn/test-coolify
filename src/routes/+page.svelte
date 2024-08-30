a9e141189122b9379aeefa90b402a862eb0d8f9a
lolxnn <inkyincolto@gmail.com>
2024-08-30 23:02:34 +0200

    Test Prisma

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,38 @@
 <script lang="ts">
-	let count = $state(0);
-
-	function increment() {
-		count += 1;
-	}
+	const { data } = $props();
 </script>
 
 <main>
-	<h1>Test Svelte 5</h1>
-	<button type="button" onclick={increment}>
-        Clicked {count} times
-    </button>
+	<h1>Users</h1>
+	<table>
+		<thead>
+			<tr>
+				{#each Object.keys(data.feed[0]) as key}
+					<th>{key}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.feed as user}
+				<tr>
+					{#each Object.values(user) as value}
+						<td>{value}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </main>
+
+<style>
+	table {
+		width: 100%;
+		border-collapse: collapse;
+	}
+
+	th,
+	td {
+		border: 1px solid #ddd;
+		padding: 8px;
+	}
+</style>
