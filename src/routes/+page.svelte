e446b54d96414969d9574a7894af4d08ab79d276
lolxnn <inkyincolto@gmail.com>
2024-08-30 20:24:40 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	let count = $state(0);
+
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<main>
+	<h1>Test Svelte 5</h1>
+	<button type="button" onclick={increment}>
+        Clicked {count} times
+    </button>
+</main>
