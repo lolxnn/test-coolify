1fff00b91a1eb26266536b397071fb020f773449
lolxnn <inkyincolto@gmail.com>
2024-09-01 14:54:28 +0200

    Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,12 @@
 <script lang="ts">
-	const { data } = $props();
+
 </script>
 
 <main>
-	<h1>Users</h1>
-	<table>
-		<thead>
-			<tr>
-				{#each Object.keys(data.feed[0]) as key}
-					<th>{key}</th>
-				{/each}
-			</tr>
-		</thead>
-		<tbody>
-			{#each data.feed as user}
-				<tr>
-					{#each Object.values(user) as value}
-						<td>{value}</td>
-					{/each}
-				</tr>
-			{/each}
-		</tbody>
-	</table>
+  <h1>Page</h1>
+  <p>This is a page.</p>
 </main>
 
 <style>
-	table {
-		width: 100%;
-		border-collapse: collapse;
-	}
 
-	th,
-	td {
-		border: 1px solid #ddd;
-		padding: 8px;
-	}
 </style>
